a6d0ace6dd432af0bde96e6a21744f850f8c126e
Patricio Yu <yupatricio0@gmail.com>
2023-05-09 23:59:56 -0300

    skin command update

--- extensions/valo.py
+++ extensions/valo.py
@@ -1,4 +1,5 @@
 import lightbulb
+import hikari
 import valorant
 import os
 from dotenv import load_dotenv
@@ -15,19 +16,15 @@ client = valorant.Client(
 @lightbulb.command("skins", "gets player info")
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def playerInfo(ctx: lightbulb.Context):
-#    account = client.get_user_by_name(ctx.options.name)
-#
-#    match = account.matchlist().history.find(queueId="competitive")
-#    
-#    if match == None:
-#        print("No Ranked match in recent history!")
-#        exit(1)
-#    else:
-#        match = match.get()
+
     skins = client.get_skins()
 
     for skin in skins:
-       print(f"{skin.name}\n")
+        embed = hikari.Embed(title="Skins", description=f"{skin.name}")
+        
+        await ctx.respond(embed)
+
+
 
 def load(bot: lightbulb.BotApp) -> None:
   bot.add_plugin(plugin)
