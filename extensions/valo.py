d6564e542b640453e34ea11eac24647cbd09fd54
patricio yu <yupatricio0@gmail.com>
2023-05-11 11:51:26 -0300

    added nav buttons to skins command

--- extensions/valo.py
+++ extensions/valo.py
@@ -2,7 +2,11 @@ import lightbulb
 import hikari
 import valorant
 import os
+import miru
+from miru.ext import nav
 from dotenv import load_dotenv
+
+load_dotenv()
 
 plugin = lightbulb.Plugin("Valorant Plugin")
 
@@ -11,20 +15,39 @@ client = valorant.Client(
     locale=None
 )
 
+class MyNavButton(nav.NavButton):
+    # This is how you can create your own navigator button
+    # The extension also comes with the following nav buttons built-in:
+    #
+    # FirstButton - Goes to the first page
+    # PrevButton - Goes to previous page
+    # IndicatorButton - Indicates current page number
+    # StopButton - Stops the navigator session and disables all buttons
+    # NextButton - Goes to next page
+    # LastButton - Goes to the last page
+
+    async def callback(self, ctx: miru.ViewContext) -> None:
+        await ctx.respond("You clicked me!", flags=hikari.MessageFlag.EPHEMERAL)
+
+    async def before_page_change(self) -> None:
+        # This function is called before the new page is sent by
+        # NavigatorView.send_page()
+        self.label = f"Page: {self.view.current_page+1}"
+
 @plugin.command
-#@lightbulb.option("name", "Name of the player", type=str, required=True)
-@lightbulb.command("skins", "gets player info")
+@lightbulb.command("skins", "shows skins")
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def playerInfo(ctx: lightbulb.Context):
-
+    embeds = []
     skins = client.get_skins()
 
     for skin in skins:
-        embed = hikari.Embed(title="Skins", description=f"{skin.name}")
+        embeds.append(hikari.Embed(title="Skins", description=f"{skin.name}")) 
         
-        await ctx.respond(embed)
-
-
+    # Define our navigator and pass in our list of pages
+    navigator = nav.NavigatorView(pages=embeds)
+    # You may also pass an interaction object to this function
+    await navigator.send(ctx.channel_id)
 
 def load(bot: lightbulb.BotApp) -> None:
   bot.add_plugin(plugin)
